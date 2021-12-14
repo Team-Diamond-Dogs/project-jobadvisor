2c2dd3c62e7c2e7ff2bbfa14a91d28169c7f42c5
Ricardo <infojobrc@gmail.com>
2021-12-14 01:47:49 -0300

    added endpoint to get basic information

--- api/src/services/getonboard.service.ts
+++ api/src/services/getonboard.service.ts
@@ -1,12 +1,53 @@
-import { HttpService } from "@nestjs/axios";
-import { JobsService } from "./interfaces/jobs-service.interface";
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom, map } from 'rxjs';
+import { JobsService } from './interfaces/jobs-service.interface';
 
-export class GetOnBoardService implements JobsService{
-    constructor(private readonly endpointUrl: string, private readonly httpService: HttpService) {
-    }
+export class GetOnBoardService implements JobsService {
+  constructor(
+    private readonly endpointUrl: string,
+    private readonly httpService: HttpService,
+  ) {}
 
-    async getJobs(): Promise<any> {}
-    async getCategories(): Promise<any> {}
-    async getTags(): Promise<any> {}
-
+  async getJobs(): Promise<any> {}
+  async getCategories(): Promise<any> {}
+  async getTags(): Promise<any> {
+    const response = await lastValueFrom(
+      this.httpService
+        .get('https://www.getonbrd.com/api/v0/tags', {
+          headers: {
+            Accept: 'application/json',
+          },
+        })
+        .pipe(map((response) => response.data.data)),
+    );
+    const filter = [];
+    response.forEach((element) => {
+      if (element.attributes.keywords !== '' && element.attributes.keywords) {
+        const filterElement = element.attributes.keywords
+          .split(',')
+          .map((value) => value.trim())
+          .filter((value) => value);
+        filter.push(...filterElement);
+      }
+    });
+    return filter;
+  }
+  async getSeniorities(): Promise<any> {
+    return this.httpService
+      .get('https://www.getonbrd.com/api/v0/seniorities', {
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+      .pipe(map((response) => response.data.data));
+  }
+  async getModalities(): Promise<any> {
+    return this.httpService
+      .get('https://www.getonbrd.com/api/v0/modalities', {
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+      .pipe(map((response) => response.data.data));
+  }
 }
--- api/src/services/interfaces/jobs-service.interface.ts
+++ api/src/services/interfaces/jobs-service.interface.ts
@@ -1,5 +1,7 @@
 export abstract class JobsService {
-    abstract getJobs(): Promise<any>;
-    abstract getCategories(): Promise<any>;
-    abstract getTags(): Promise<any>;
+  abstract getJobs(): Promise<any>;
+  abstract getCategories(): Promise<any>;
+  abstract getTags(): Promise<any>;
+  abstract getSeniorities(): Promise<any>;
+  abstract getModalities(): Promise<any>;
 }
