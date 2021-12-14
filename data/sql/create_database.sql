b07e45afc899fcfcc06633104add75939767dd95
José González <hello.zeegcl@gmail.com>
2021-12-14 16:04:28 -0300

    fix(data): missing column attribute in tag_courses table

--- data/sql/create_database.sql
+++ data/sql/create_database.sql
@@ -12,7 +12,7 @@ COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE tag_courses (
-	`id` INTEGER NOT NULL,
+	`id` INTEGER auto_increment NOT NULL,
 	`tag_id` INTEGER NOT NULL,
 	`name` varchar(200) NOT NULL,
 	`url` varchar(200) NOT NULL,
