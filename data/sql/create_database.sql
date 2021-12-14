0ce50db8d1a96fc638ccd2f9da3390b6d6cd7ef7
José González <800655+ZeegCL@users.noreply.github.com>
2021-12-14 09:14:28 -0600

    chore: update develop from main (#8)
    * feat(data): Add docs and scripts for the Tag model
    
    * feat(data): Add docs and scripts for Tag Courses
    
    * fix(data): update columns in tags table to be less verbose
    
    Also, wrap column names in backticks

---
+++ data/sql/create_database.sql
@@ -0,0 +1,26 @@
+CREATE TABLE tags (
+	`id` INTEGER auto_increment NOT NULL,
+	`name` varchar(100) NOT NULL,
+	`value` varchar(100) NOT NULL,
+	`keywords` varchar(200) NULL,
+	`is_skill` BOOL DEFAULT 1 NULL,
+	CONSTRAINT tags_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE tag_courses (
+	`id` INTEGER NOT NULL,
+	`tag_id` INTEGER NOT NULL,
+	`name` varchar(200) NOT NULL,
+	`url` varchar(200) NOT NULL,
+	`thumbnail_url` varchar(200) NOT NULL,
+	`priority` SMALLINT DEFAULT 0 NOT NULL,
+	CONSTRAINT tag_courses_PK PRIMARY KEY (id),
+	CONSTRAINT tag_courses_FK FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
