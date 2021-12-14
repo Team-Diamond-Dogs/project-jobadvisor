c94f2b1b2548b6b46ea6498ab3fdc15602a8e0cd
David Quezada <davidquezada@David-Quezadas-MacBook-Pro.local>
2021-12-13 22:10:08 -0300

    JobsController decorator

--- api/src/controllers/jobs.controller.ts
+++ api/src/controllers/jobs.controller.ts
@@ -1,7 +1,8 @@
-import { Inject } from "@nestjs/common";
+import { Controller, Inject } from "@nestjs/common";
 import { CourseService } from "src/services/interfaces/course-service.interface";
 import { JobsService } from "src/services/interfaces/jobs-service.interface";
 
+@Controller()
 export class JobsController {
     constructor(
         @Inject('JOBS_SERVICE') private jobsService: JobsService,
