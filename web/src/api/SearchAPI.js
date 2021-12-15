b3ec2a1ae3c6e1d8bc38386f0070f6ab8caea2cf
José González <hello.zeegcl@gmail.com>
2021-12-14 23:29:17 -0300

    fix(web): remove mock data and do calls against the api

--- web/src/api/SearchAPI.js
+++ web/src/api/SearchAPI.js
@@ -1,48 +1,34 @@
 import axios from 'axios';
-import mock_job_search from './mock-job-search';
-import mock_skills from "./mock-skills";
-import mock_skill_search from './mock-skill-search';
 
 class SearchAPI {
     constructor() {
         this.api_url = process.env.REACT_APP_API_BASE_URL;
     }
 
-    async searchJob(jobName, seniority = null) {
+    async searchJob(jobName) {
         if (jobName === undefined || jobName === null || jobName === "") {
             throw new Error("Se debe especificar un término para el nombre del cargo.");
         }
 
-        let queryParams = { job: jobName };
-        if (seniority !== undefined && seniority !== null) {
-            queryParams["seniority"] = seniority;
-        }
-        
-        /*
-        const response = await axios.get(`${this.api_url}/search`, { params: queryParams });
+        const response = await axios.get(`${this.api_url}/jobs/keyword/${jobName}`);
 
         return response.data;
-        */
-
-        return mock_job_search;
     }
 
-    async searchSkill(skills, seniority) {
+    async searchSkill(skills) {
         if (skills === undefined || skills === null || skills.length === 0) {
             throw new Error("Se debe especificar al menos una habilidad para realizar la búsqueda.");
         }
 
-        /*
         const response = await axios.get(`${this.api_url}/search`);
 
         return response.data;
-        */
-
-        return mock_skill_search;
     }
 
     async getSkillsList() {
-        return mock_skills;
+        const response = await axios.get(`${this.api_url}/tags`);
+
+        return response.data;
     }
 
     async getSenioritiesList() {
